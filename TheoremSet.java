9e4623cf2dc15cea382d73f68d3f34a9e5740cee
shanfran <shanfran@berkeley.edu>
2013-07-28 12:55:20 -0700

    Update TheoremSet.java

--- TheoremSet.java
+++ TheoremSet.java
@@ -1,9 +1,40 @@
+import java.util.*;
+
 public class TheoremSet {
+	
+	    private final HashMap<String, Expression> theorems; //Creates HashMap with objects
 
-	public TheoremSet ( ) {
-	}
+        public TheoremSet ( ) {
+        	theorems = new HashMap<String, Expression >( ); //Map to store values
+        }
+        
+        public boolean contains(final String s) { //return true if contains string for key
+        	return this.theorems.containsKey(s);
+        }
+	    
 
-	public Expression put (String s, Expression e) {
-		return null;
-	}
-}
+       // public Expression put (String s, Expression e) {
+                //return null;
+        // }
+        
+        public void put (final String s, Expression e) { //Store string value
+	    	this.theorems.put(s, e);
+	    	
+        }
+        
+        public String getAsString(final String key) { //returns the key corresponding to the string
+            final Object retrieved = this.theorems.get(key);
+            return retrieved instanceof String ? (String) retrieved : null;
+        }
+        
+        public final HashMap<String, Expression> transfer( ) { //transfer
+        	return theorems;
+        }
+        	
+        }
+        
+        
+        
+        
+
+
